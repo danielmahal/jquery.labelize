7bb9fedaa23f9855d6d8f36f7aa811b4af9337e5
Daniel <danielmahal@gmail.com>
2012-12-09 15:04:43 +0100

    Change to using classes instead of operating directly on elements. Add example

--- src/jquery.labelize.js
+++ src/jquery.labelize.js
@@ -1,46 +1,26 @@
-/* Labelizer jQuery plugin by Daniel Mahal (http://github.com/danielmahal) */
+/* Labelizer jQuery plugin by Daniel Mahal and Simen Brekken (http://github.com/danielmahal) */
 
 (function($) {
     $.fn.labelize = function(options) {
         var settings = {
-            animate: true,
-            fadeSpeed: 150,
-            fadedOpacity: 0.35
+            focusClass: 'has-focus',
+            valueClass: 'has-value'
         };
 
         $.extend(settings, options);
 
-        var setOpacity = function(label, opacity, animate) {
-            animate = animate && settings.animate;
-
-            if(animate) {
-                label.stop().fadeTo(settings.fadeSpeed, opacity);
-            } else {
-                label.css('opacity', opacity);
-            }
-        };
-
         return $(this).each(function() {
-            var input = $(this);
-            var label = $('label[for=' + input.attr('id') + ']');
+            var $input = $(this);
+            var $label = $('label[for=' + $input.attr('id') + ']');
+            var $elements = $input.add($label);
 
-            var check = function(e, animate) {
-                animate = animate === false ? false : true;
+            $input.on('keyup change', function(e) {
+                $elements.toggleClass(settings.valueClass, !!$input.val().length)
+            });
 
-                var hasValue = input.val().length > 0;
-                var hasFocus = (e && e.type === 'focus') || input.is(':focus');
-
-                if(hasValue) {
-                    setOpacity(label, 0, animate);
-                } else if(hasFocus) {
-                    setOpacity(label, settings.fadedOpacity, animate);
-                } else {
-                    setOpacity(label, 1, animate);
-                }
-            };
-
-            input.bind('focus blur keyup change', check);
-            check(null, false);
+            $input.on('focus blur', function() {
+                $elements.toggleClass(settings.focusClass, !!$input.is(':focus'));
+            });
         });
-    };
+    }
 })(jQuery);
